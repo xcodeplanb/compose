be29d0dc022aad037eb91a48c2720a18f2523598
chobo82kr <chobo82kr@podbbang.com>
2025-03-04 11:28:29 +0900

    remove unused

--- mobile/src/main/java/com/makeshop/podbbang/PodbbangApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.makeshop.podbbang
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PodbbangApplication : Application() {
-
-}
--- mobile/src/main/java/com/makeshop/podbbang/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.makeshop.podbbang.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Color121212 = Color(0xFF121212)
-val Color1A1A1A = Color(0xFF1A1A1A)
-val Color666666 = Color(0xFF666666)
-val ColorFFFFFF  = Color(0xFFFFFFFF)
